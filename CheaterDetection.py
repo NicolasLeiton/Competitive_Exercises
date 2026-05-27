f493364c028a874f4380aca1d8817d41262f864d
NicolasLeiton <leiton0209@gmail.com>
2026-05-27 15:44:40 -0500

    Correcion cheaterDetection

--- CheaterDetection.py
+++ CheaterDetection.py
@@ -1,7 +1,8 @@
 n = int(input())
 
 for i in range(n):
-    num = int(float(input())*1000)
+    entrada = float(input())
+    num = int(round(entrada * 1000))
     if num%15==0 or (num+5)%15==0:
         print("VALID")
     else:
